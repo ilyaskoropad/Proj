1fda56d8cf6a51fbcda11f071d98ba7f010d1da4
Ilya <ilya.skoropad0@gmail.com>
2016-10-19 11:21:29 +0300

    Практические

---
+++ PHP/Proj 1/Entrance.php
@@ -0,0 +1,14 @@
+﻿<?php 
+	
+	mb_internal_encoding("UTF-8");
+	$login = $_POST['_login'];
+	$password = $_POST['_password'];
+	
+	$TryLogin = "admin";
+	$TryPassword = "0000";
+		
+	if($login == $TryLogin && $password == $TryPassword)
+	{
+		print "Вы хацкер?";
+	}
+?>
---
+++ Python/first.py
@@ -0,0 +1,13 @@
+﻿def StrRevers(string):
+	index = len(string);
+	result = "Result: ";
+
+	for index in range(index, 0, -1):
+		index = index - 1;
+		result = result + string[index];
+	
+	print result;
+	
+print u'Введеная строка будет развернута. >';
+input_st = raw_input();
+StrRevers(input_st);
